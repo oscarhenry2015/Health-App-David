e5e12f393273764ca8eca2f870c65f79c65731ce
PC <winniewaiguru@gmail.com>
2025-03-09 11:29:04 +0300

    header addition

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
     },
   })
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-// alert("I am working!!");
+const menuIcon = document.querySelector(".menu-icon");
+const sideMenu = document.querySelector(".side-menu");
+const closeIcon = document.querySelector("#close-btn");
+
+// Open the side menu
+menuIcon.addEventListener("click", function () {
+  sideMenu.classList.add("active");
+});
+
+// Close the side menu
+closeIcon.addEventListener("click", function () {
+  sideMenu.classList.remove("active");
+});
